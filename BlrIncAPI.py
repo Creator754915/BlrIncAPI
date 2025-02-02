c22774eaa437c8c9d90c3b73fd33e875a7d77597
Creator754915 <106489587+Creator754915@users.noreply.github.com>
2025-02-02 22:38:52 +0100

    Update BlrIncAPI.py

--- BlrIncAPI.py
+++ BlrIncAPI.py
@@ -1,14 +1,28 @@
 import requests
+from tabulate import tabulate
 
 
-def RequestBLR():
-    url = "https://9d330c61-5751-4a4b-996b-cdb372d3dfe2-00-181ng4cjfs9fd.spock.replit.dev/api/v1/test.json"
+def displayTable(data):
+    headers = ["Username", "Email", "Solde", "Description", "Date"]
+    table = [[user["username"], user["email"], user["solde"], user["description"], user["date"]] for user in data]
+    print(tabulate(table, headers=headers, tablefmt="grid"))
+
+
+def RequestBLR(type="users"):
+    UserURL = "https://9d330c61-5751-4a4b-996b-cdb372d3dfe2-00-181ng4cjfs9fd.spock.replit.dev/api/v1/users.json"
+    MarketURL = "https://9d330c61-5751-4a4b-996b-cdb372d3dfe2-00-181ng4cjfs9fd.spock.replit.dev/api/v1/market.json"
+    url = ""
+
+    if type == "users":
+        url = UserURL
+    elif type == "market":
+        url = MarkerURL
+    
     try:
         response = requests.get(url)
 
         if response.status_code == 200:
             data = response.json()
-            print("Données reçues :", data)
             return data, response.status_code
         else:
             return response.status_code
@@ -16,7 +30,7 @@ def RequestBLR():
         return e
 
 
-def PrintPretty(data):
+def printPretty(data):
     print("Informations utilisateur :")
     print("-" * 30)
     for key, value in data.items():
@@ -29,25 +43,43 @@ def PrintPretty(data):
 class getUser:
     def __init__(self):
         super().__init__()
-        self.data, self.status = RequestBLR()
+        self.data, self.status = RequestBLR("users")
 
     def all(self):
         if self.CheckStatus():
-            for user in self.data:
-                return user
+            return self.data["data"]
         else:
             raise ConnectionError
 
     def name(self, username):
         for user in self.data["data"]:
             if user["username"] == username:
-                PrintPretty(user)
+                return user
 
-    def sortBy(self, type):
-        pass
+    def sortBy(self, types, order="asc"):
+        if self.CheckStatus():
+            if types in ["username", "solde", "date"]:
+                reverse = order == "desc"
+                return sorted(self.data["data"], key=lambda x: x[types], reverse=reverse)
+            else:
+                raise ValueError("Type de tri non supporté")
+        else:
+            raise ConnectionError("Échec de connexion à l'API")
 
     def CheckStatus(self):
         if self.status == 200:
             return True
         else:
             return False
+
+
+class getMarketCap:
+    def __init__(self):
+        super().__init__()
+        self.data, self.status = RequestBLR("market)
+
+
+class getTransaction:
+    def __init__(self):
+        super().__init__()
+        
